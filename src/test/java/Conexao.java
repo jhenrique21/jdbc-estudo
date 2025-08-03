5bdc10c0fdec4d63960e96bcb665a274334c8c01
João Henrique <joaokocssis@gmail.com>
2025-08-03 19:26:45 -0300

    conexao com o bd

---
+++ src/test/java/Conexao.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static void main(String[] args) throws SQLException {
+        Connection conexao = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost/world", "usuario", "senha");
+            ResultSet rsCliente = conexao.createStatement().executeQuery("SELECT * FROM city limit 20");
+            while (rsCliente.next())
+            {
+                System.out.println("Nome: " + rsCliente.getString("Name"));
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (conexao != null)
+            {
+                conexao.close();
+            }
+
+        }
+    }
+}
