7e05d969a9f24cd23576992fbfd191ccd1c9a505
João Henrique <joaokocssis@gmail.com>
2025-08-03 19:39:23 -0300

    uso variaveis para bd

--- src/test/java/Conexao.java
+++ src/test/java/Conexao.java
@@ -4,11 +4,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conexao {
+
     public static void main(String[] args) throws SQLException {
         Connection conexao = null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conexao = DriverManager.getConnection("jdbc:mysql://localhost/world", "usuario", "senha");
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost/world", System.getenv("USERBD"), System.getenv("PASS_MYSQL"));
             ResultSet rsCliente = conexao.createStatement().executeQuery("SELECT * FROM city limit 20");
             while (rsCliente.next())
             {
